71bacadcb001effae8108f1498f091031455f8da
Dong-jin Ahn <adoji92@gmail.com>
2019-01-09 21:46:08 +0900

    Start LeetCode Problem5

---
+++ src/main/scala/LeetCode/LcProblem5.scala
@@ -0,0 +1,14 @@
+package LeetCode
+
+import scala.collection.mutable.ListBuffer
+
+object LcProblem5 extends App {
+  def longestPalindrome(s: String): String = {
+    val buffer = new ListBuffer[Char]()
+
+    ""
+  }
+
+  println(longestPalindrome("babad"))
+  println(longestPalindrome("cbbd"))
+}
