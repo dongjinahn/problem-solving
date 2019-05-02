0e96ad5606c0c68ee940aa639bd6efdedb5fc9ab
Dong-jin Ahn <qwerfrewq@naver.com>
2019-05-02 17:01:06 +0900

    LeetCode 938

---
+++ python/leetcode/problem938.py
@@ -0,0 +1,17 @@
+class TreeNode(object):
+  def __init__(self, x):
+    self.val = x
+    self.left = None
+    self.right = None
+
+def rangeSumBST(root, L, R):
+  if root == None:
+    return 0
+  if root.val < L:
+    return self.rangeSumBST(root.right, L, R)
+  if root.val > R:
+    return self.rangeSumBST(root.left, L, R)
+  leftSum = self.rangeSumBST(root.left, L, R)
+  rightSum = self.rangeSumBST(root.right, L, R)
+  selfSum = root.val if root.val >= L and root.val <= R else 0
+  return leftSum + selfSum + rightSum
