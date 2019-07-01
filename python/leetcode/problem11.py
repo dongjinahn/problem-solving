b31ed626d1aebb0a27101f0e590820059349772e
dongjin.ahn <adoji92@gmail.com>
2019-07-01 15:09:26 +0900

    Convert scala leetcode 11 to python

---
+++ python/leetcode/problem11.py
@@ -0,0 +1,17 @@
+class Solution:
+  def maxArea(self, heightList: List[int]) -> int:
+    max = 0
+    a = 0
+    b = len(heightList) - 1
+    while a != b:
+      heightA = heightList[a]
+      heightB = heightList[b]
+      area = min([heightA, heightB]) * (b - a)
+      if area > max:
+        max = area
+      if heightB > heightA:
+        a = a +  1
+      else:
+        b = b - 1
+    return max
+
