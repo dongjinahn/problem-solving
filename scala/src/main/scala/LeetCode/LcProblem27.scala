5a644393dff58a6d3cafce8670e82a2ced25093c
dongjin.ahn <adoji92@gmail.com>
2019-07-16 16:12:36 +0900

    py 17, scala 27

---
+++ scala/src/main/scala/LeetCode/LcProblem27.scala
@@ -0,0 +1,15 @@
+object Solution {
+  def removeElement(nums: Array[Int], value: Int): Int = {
+    var i = 0
+    var n = nums.length
+    while (i < n) {
+      if (nums(i) == value) {
+        nums(i) = nums(n - 1)
+        n -= 1
+      } else {
+        i += 1
+      }
+    }
+    n
+  }
+}
