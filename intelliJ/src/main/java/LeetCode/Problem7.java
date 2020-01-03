15ce9a12d80fcdf9cdda6143d639643106258a20
Dong-jin Ahn <adoji92@gmail.com>
2020-01-03 16:51:11 +0900

    LeetCode7.java

---
+++ intelliJ/src/main/java/LeetCode/Problem7.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+public class Problem7 {
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+        System.out.println(reverse(-123));
+        System.out.println(reverse(120));
+        System.out.println(reverse(1534236469));
+    }
+
+    public static int reverse(int x) {
+        int res = 0;
+        boolean hasError = false;
+        while (x != 0) {
+            try {
+                res = Math.multiplyExact(res, 10);
+                res = Math.addExact(res, x % 10);
+            } catch (ArithmeticException e) {
+                hasError = true;
+            }
+            if (hasError) {
+                // try catch중에 return하는건 anti-pattern
+                return 0;
+            }
+            x = x / 10;
+        }
+        return res;
+    }
+}
