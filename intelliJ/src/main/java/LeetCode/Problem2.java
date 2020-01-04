9ab2d98b771dc41fd7c97c119c837e5151b5a2cf
Dong-jin Ahn <adoji@Dong-jinui-MacBookPro.local>
2020-01-04 13:37:05 +0900

    Extract ListNode

--- intelliJ/src/main/java/LeetCode/Problem2.java
+++ intelliJ/src/main/java/LeetCode/Problem2.java
@@ -50,23 +50,4 @@ public class Problem2 {
         }
         return dummy.next;
     }
-
-    public static class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
-
-        @Override
-        public String toString() {
-            final StringBuilder sb = new StringBuilder(50);
-            ListNode cur = this;
-            sb.append(this.val);
-            while (cur.next != null) {
-                sb.append("->");
-                sb.append(cur.next.val);
-                cur = cur.next;
-            }
-            return sb.toString();
-        }
-    }
 }
---
+++ intelliJ/src/main/java/LeetCode/Problem21.java
@@ -0,0 +1,7 @@
+package LeetCode;
+
+public class Problem21 {
+    public static void main(String[] args) {
+
+    }
+}
