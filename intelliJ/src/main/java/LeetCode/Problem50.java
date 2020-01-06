a093be90c1ac9069fb6ab84472f0a39664a92a68
Dong-jin Ahn <adoji92@gmail.com>
2020-01-06 10:02:26 +0900

    LeetCode50.java

---
+++ intelliJ/src/main/java/LeetCode/Problem50.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class Problem50 {
+    public static void main(String[] args) {
+        System.out.println(myPow(2, 10));
+        System.out.println(myPow(2.1, 3));
+        System.out.println(myPow(2, Integer.MIN_VALUE));
+        System.out.println(myPow(2, -2));
+    }
+    public static double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            x = 1 / x;
+            return x * myPow(x, -(n + 1));
+        }
+        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+    }
+}
