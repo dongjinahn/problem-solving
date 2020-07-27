938426cd2a4937fa08af215937e68b54d83f2856
Dong-jin Ahn <adoji92@gmail.com>
2020-07-27 14:02:26 +0900

    LeetCode617.java

---
+++ intelliJ/src/main/java/LeetCode/Problem617.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+public class Problem617 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+        final TreeNode root = new TreeNode();
+        root.val = t1 == null ? t2.val :
+                t2 == null ? t1.val : t1.val + t2.val;
+
+        root.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+        root.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+        return root;
+    }
+}
